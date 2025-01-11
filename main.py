aca148bc4f21de2e4cad2245fb919e2937e89b9e
Nematjon <rasulovn835@gmail.com>
2025-01-12 00:38:49 +0500

    5oy 4-dars main

--- main.py
+++ main.py
@@ -1,78 +1,101 @@
-# 1-misol
+from database import db
 
-from decimal import Decimal
-from random import random
-import random
+def show_models():
+    for model in db.select_all_models():
+        print(f"{model[0]}. Name: {model[1]}")
 
-class DEC:
-    def __set_name__(self, owner, name):
-         self.name = "_" + name
+def a(name: str, count: int):
+    return name.center(count, " ") + ' ' + '|'
 
-    def __set__(self, instance, value: Decimal):
-         if type(value) is str and value.isdecimal():
-             instance.__dict__[self.name] = value
-         else:
-             try:
-                raise ValueError("Decimal tipida kiritilmadi!!!")
-             except ValueError as e:
-                 print(e)
+def show_cars(model_id: int):
+    print('-' * 145)
+    print('|', a("ID", 8), a("NOMI", 102), a("QUVVATI", 8), a("NARXI", 8), a("MODEL_ID", 8))
+    for car in db.select_cars_by_model_id(model_id):
+        pass
+    print('-' * 145)
 
-    def __get__(self, instance, owner):
-        return instance.__dict__[self.name]
+def show_colors():
+    for color in db.select_colors():
+        print(f"{color[0]}. {color[1]}")
 
-sanalar = []
+def show_list_employees():
+    for employee in db.select_employees():
+        print(f"{employee[0]}. {employee[1]}")
 
-class Valyuta:
-    summa = DEC()
-    def __init__(self, summa: str):
-        self.summa = summa
+def show_info_employee(employee_id: int):
+    for emplyee in db.show_info_employee(employee_id):
+        print(f"{emplyee[0]}. {emplyee[1]}\n"
+              f"Maosh: {emplyee[2]}\n"
+              f"Lavozimi: {emplyee[3]}")
 
-        for i in range(31):
-            sanalar.append(f"2024.12.{i}")
 
-    def konfer(self):
-        som = Decimal(self.summa) * Decimal("12000")
-        return f"{som} UZS (Sana: {random.choice(sanalar)})"
+def run():
+    while True:
+        command = input("Buyruqni kiriting: ").lower()
+        if command == 'stop':
+            print("Dastur to'xtadi!!!")
+            break
+        elif command == 'add model':
+            while True:
+                model = input("Avtomobil modelini kiriting (exit - chiqish)")
+                if model.lower() == 'exit':
+                    break
 
-d = Valyuta("99")
-print(d.konfer())
+                db.insert_model(model)
+                print("Model qo'shildi✅")
+        elif command == 'add car':
+            while True:
+                name = input("Avtomobil nomini kiriting (exit - chiqish): ")
 
-# 2 - misol
+                if name.lower() == 'exit':
+                    break
 
-from decimal import Decimal
-from random import random
-import random
+                power = input("Avtomobil ot kuchini kiriting: ")
+                price = input("Avtomobil narxini kiriting: ")
 
-class Dec:
-    def __set_name__(self, owner, name):
-         self.name = "_" + name
+                show_models()
 
-    def __set__(self, instance, value: Decimal):
-         if type(value) is str and value.isdecimal() and value < Decimal("0"):
-             instance.__dict__[self.name] = value
-         else:
-             try:
-                raise ValueError("Decimal tipida kiritilmadi!!!")
-             except ValueError as e:
-                 print(e)
+                model_id = input("Avtomobil modelini id sini kiriting: ")
 
-    def __get__(self, instance, owner):
-        return instance.__dict__[self.name]
+                show_colors()
 
-sanalar = []
+                color_id = input("Rangni tanlang (rangni id raqamini kiriting): ")
+                if power.isdigit() and price.isdigit() and model_id.isdigit():
+                    db.insert_car(name, int(power), float(price), int(model_id), int(color_id))
+                    print("Car qo'shildi✅✅")
+        elif command == 'show cars':
+            show_models()
+            model_id = input('Model id ni kiriting: ')
+            if model_id.isdigit():
+                show_cars(int(model_id))
+        elif command == 'add employee':
+            while True:
+                name = input("Ishchining ism va familiyasini kiriting (exit - chiqish): ")
+                if name == 'exit':
+                    break
+                oylik = input("Oylik maoshni kiriting: ")
+                lavozimi = input("Lavozimni kiriting: ")
+                if oylik.isdigit():
+                    db.insert_employee(name, int(oylik), lavozimi)
+                    print("ishchi qo'shildi✅✅✅")
+        elif command == 'list employees':
+            show_list_employees()
+        elif command == 'show info employee':
+            show_list_employees()
+            employee_id = input("ishchining id sini kiriting: ")
+            if employee_id.isdigit():
+                show_info_employee(int(employee_id))
+            else:
+                print("ishchining id sini faqat raqamda kiriting!!")
 
-class Mahsulot:
-    def __init__(self, mahsulot_narxi):
-        self.mahsulot_narxi = mahsulot_narxi
-        for i in range(31):
-            sanalar.append(f"2024.12.{i}")
+if __name__ == '__main__':
+    # db.drop_table_models()
+    db.create_table_models()
 
-    def chegirma(self):
-        chegirma = random.randint(1, 50)
-        chegirma = Decimal(chegirma)
-        yangi_narx = Decimal(self.mahsulot_narxi) - ((Decimal(self.mahsulot_narxi) / Decimal("100")) * Decimal(chegirma))
-        return (f"Mahsulot narxi: {self.mahsulot_narxi}, chegirma: {chegirma}%\n"
-                f"Yangi narx: {yangi_narx} UZS (Sana: {random.choice(sanalar)})")
+    db.create_table_colors()
 
-p = Mahsulot("200000")
-print(p.chegirma())
+    db.create_table_cars()
+
+    db.create_table_employees()
+
+    run()
