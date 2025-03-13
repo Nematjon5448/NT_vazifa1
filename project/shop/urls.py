1aed1626eceddbe1581f0d2042ad16af9f4c53ed
Nematjon <rasulovn835@gmail.com>
2025-03-13 12:14:14 +0500

    7-oy 7-dars

--- project/shop/urls.py
+++ project/shop/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, SingleProduct, CommentSaqlash
 
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path('', IndexView.as_view(), name='index'),
+    path('single_product/<int:product_id>', SingleProduct.as_view(), name='single_product'),
+    path('single_product/<int:product_id>/comment', CommentSaqlash.as_view(), name='comment_saqlash'),
 ]
--- project/shop/views.py
+++ project/shop/views.py
@@ -1,6 +1,8 @@
+from django.urls import reverse_lazy
+
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Product, Category
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Product, Category, Comment
 
 
 class IndexView(ListView):
@@ -13,3 +15,28 @@ class IndexView(ListView):
         context['categories'] = Category.objects.filter(parent=None)
         return context
 
+class SingleProduct(DetailView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'shop/single-product.html'
+    pk_url_kwarg = 'product_id'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['comments'] = Comment.objects.filter(product=self.object.pk)
+        return context
+
+class CommentSaqlash(CreateView):
+    model = Comment
+    template_name = 'shop/single-product.html'
+    fields = ['text']
+    pk_url_kwarg = 'product_id'
+
+    def get_success_url(self):
+        return reverse_lazy('single_product', kwargs={'product_id': self.object.product.pk})
+
+    def form_valid(self, form):
+        product_id = self.kwargs.get('product_id')
+        form.instance.product = Product.objects.get(pk=product_id)
+        form.instance.user = self.request.user
+        return super().form_valid(form)
